4d240bbc96f3bcb3f9c503d58102043f64c09d3d
Emanuel Catriquil <56092756+ecatriquil@users.noreply.github.com>
2020-01-18 07:16:13 -0300

    Añade funciones
    Son necesarias para consumir la api de exchangerates y mostrar los resultados al usuario.

---
+++ src/app.js
@@ -0,0 +1,85 @@
+/// <reference types="jquery" />
+
+document.querySelector('#latest').onclick = (e) => {
+    ocultarCalendario();
+}
+
+document.querySelector('#date').onclick = (e) => {
+    mostrarCalendario();
+    actualizarCalendario();
+}
+
+document.querySelector('#submit').onclick = ((e) => {
+    e.preventDefault();
+    const { date, base } = definirBaseDate();
+    borrarResultadosAnteriores();
+    traerExchangeRates(date, base);
+});
+
+function definirBaseDate() {
+    const $base = document.querySelector('#base');
+    const base = $base.value;
+    let date;
+    if (document.querySelector('#exchange-reference').options.value === 'latest') {
+        date = document.querySelector('#exchange-reference').options.value
+    }
+    if (document.querySelector('#exchange-reference').options.value === 'date') {
+        date = document.querySelector('#start').value;
+    }
+
+    return { date, base }
+}
+
+function mostrarCalendario() {
+    const $calendario = document.querySelector('#calendario');
+    $calendario.classList.replace('oculto', 'visible');
+}
+
+function ocultarCalendario() {
+    const $calendario = document.querySelector('#calendario');
+    $calendario.classList.replace('visible', 'oculto');
+}
+
+function traerExchangeRates(date, base) {
+    fetch(`https://api.exchangeratesapi.io/${date}?base=${base}`)
+        .then(respuesta => respuesta.json())
+        .then(respuesta => {
+            $('h2').text(`Cambios del dia ${respuesta.date} en base ${respuesta.base}`);
+            Object.keys(respuesta.rates).forEach(moneda => {
+                $('ul').append($(`<li class=base>${moneda}: ${respuesta.rates[moneda]}</li>`))
+            });
+        })
+        .catch(error => console.error('FALLO', error));
+}
+
+function cargarOpcionesBase() {
+    fetch(`https://api.exchangeratesapi.io/latest`)
+        .then(respuesta => respuesta.json())
+        .then(respuesta => {
+            $('select').append($(`<option value=${respuesta.base}>${respuesta.base}</option>`));
+            Object.keys(respuesta.rates).forEach(moneda => {
+                $('select').append($(`<option value="${moneda}">${moneda}</option>`));
+            });
+        })
+        .catch(error => console.error('FALLO', error));
+}
+
+function borrarResultadosAnteriores() {
+    const $resultados = document.querySelectorAll('.base');
+    for (let i = 0; i < $resultados.length; i++) {
+        $resultados[i].remove();
+    }
+}
+
+function actualizarCalendario(){
+    var hoy = new Date();
+    var dd = String(hoy.getDate()).padStart(2, '0');
+    var mm = String(hoy.getMonth() + 1).padStart(2, '0');
+    var yyyy = hoy.getFullYear();
+    const fechaCompleta = `${yyyy}-${mm}-${dd}`;
+    let $inputCalendario = document.querySelector('#start');
+    $inputCalendario.value = fechaCompleta; 
+    $inputCalendario.max = fechaCompleta;    
+}
+
+cargarOpcionesBase();
